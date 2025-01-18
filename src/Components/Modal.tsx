fdb8cc2348c87e9f78e932e0eef5fa9d50bdcccd
developer-256 <ch256.it@gmail.com>
2025-01-18 12:32:13 +0500

    ui complete

---
+++ src/Components/Modal.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, ReactNode, SetStateAction } from "react";
+
+const Modal = ({
+  children,
+  open,
+  setOpen,
+  onContinue,
+}: {
+  children: ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  onContinue: () => void;
+}) => {
+  if (open)
+    return (
+      <section className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black/50 backdrop-blur-sm">
+        <div className="flex min-h-64 w-[600px] flex-col rounded-lg border-2 bg-white px-3 py-2.5 shadow-xl">
+          <div className="w-full flex-1">{children}</div>
+
+          <div className="flex items-center justify-end gap-3">
+            <button
+              onClick={() => setOpen(!open)}
+              className="rounded-lg bg-red-500 px-4 py-1.5 text-white shadow-md"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={onContinue}
+              className="rounded-lg bg-green-500 px-4 py-1.5 text-white shadow-md"
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+};
+
+export default Modal;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,94 @@
-import Link from "next/link";
+"use client";
+import React, { useState } from "react";
+import { Edit, PlusCircle, Trash2 } from "lucide-react";
+import Modal from "@/Components/Modal";
 
-import { LatestPost } from "@/app/_components/post";
-import { auth } from "@/server/auth";
-import { api, HydrateClient } from "@/trpc/server";
-
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
+const page = () => {
+  const [openEdit, setOpenEdit] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+  const [openNew, setOpenNew] = useState(false);
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+    <section className="mx-auto max-w-7xl">
+      <h1 className="mt-10 text-center text-2xl font-semibold">
+        Shopping List
+      </h1>
 
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
+      <div className="mt-10 grid grid-cols-4 items-center justify-center">
+        <div className="border-2 border-black/40 bg-black px-4 py-2 text-white">
+          Serial Number
+        </div>
+        <div className="col-span-2 border-2 border-black/40 border-x-white bg-black px-4 py-2 text-white">
+          Shopping Item
+        </div>
+        <div className="border-2 border-black/40 bg-black px-4 py-2 text-white">
+          <button
+            className="flex items-center gap-2 text-yellow-500"
+            onClick={() => setOpenNew(!openNew)}
+          >
+            Add New
+            <PlusCircle />
+          </button>
+        </div>
+      </div>
+
+      {Array.from({ length: 5 }).map((Item, idx) => {
+        return (
+          <div
+            key={idx}
+            className="grid grid-cols-4 items-center justify-center"
+          >
+            <div className="border-2 border-black/40 px-4 py-2">{idx + 1}.</div>
+            <div className="col-span-2 cursor-pointer border-2 border-black/40 px-4 py-2 line-through decoration-red-500">
+              Shopping Item # {idx + 1}
+            </div>
+            <div className="flex items-center gap-4 border-2 border-black/40 px-4 py-2">
+              <button onClick={() => setOpenDelete(!openDelete)}>
+                <Trash2 className="text-red-500" />
+              </button>
+
+              <button onClick={() => setOpenEdit(!openEdit)}>
+                <Edit className="text-green-700" />
+              </button>
             </div>
           </div>
+        );
+      })}
+      {/* <div className="m-2 font-semibold text-red-500">
+        No Item in the list yet
+      </div> */}
 
-          {session?.user && <LatestPost />}
-        </div>
-      </main>
-    </HydrateClient>
+      <Modal
+        open={openNew}
+        setOpen={setOpenNew}
+        onContinue={() => {
+          console.log("Hello");
+        }}
+      >
+        New
+      </Modal>
+
+      <Modal
+        open={openEdit}
+        setOpen={setOpenEdit}
+        onContinue={() => {
+          console.log("Hello");
+        }}
+      >
+        Edit
+      </Modal>
+
+      <Modal
+        open={openDelete}
+        setOpen={setOpenDelete}
+        onContinue={() => {
+          console.log("Hello");
+        }}
+      >
+        Delete
+      </Modal>
+    </section>
   );
-}
+};
+
+export default page;
